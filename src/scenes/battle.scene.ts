b0e16f572a836a54848384e143fb0b1a2a9ae9ae
Ratko Ćosić <ratko_cosic@hotmail.com>
2018-08-09 15:29:47 +0200

    skirmish mode started

--- src/scenes/battle.scene.ts
+++ src/scenes/battle.scene.ts
@@ -1,4 +1,12 @@
+import { Settings } from "../models/settings";
+
 export class BattleScene extends Phaser.Scene {
+
+    private _options: any;
+    private _music: Phaser.Sound.BaseSound;
+    private _soundRound: Phaser.Sound.BaseSound;
+    private _soundClick: Phaser.Sound.BaseSound;
+    private _isTurnInProgress: boolean;
 
     constructor() {
         super({
@@ -7,8 +15,32 @@ export class BattleScene extends Phaser.Scene {
     }
 
     init(data): void {
+        this._options = data.options || {};
+        this._isTurnInProgress = false;
+    }
+
+    preload(): void {
+    }
+
+    create(): void {
+        var music;
+        // check if music is enabled
+        if (Settings.sound.musicVolume > 0) {
+            // introductory fade in of theme music
+            this.sound.stopAll();
+            this._music = this.sound.add('battle_' + this._options.terrain);
+            music = this._music.play('', { loop: true });    
+        }
+
+        // add sounds
+
+        this._soundRound = this.sound.add('gong');
+        this._soundClick = this.sound.add('click');
 
     }
 
+    update(): void {
 
+    }
+    
 }
