fa38e2aa6abfd398110ac5f4b6efb01f4f1b44c0
Dedalus Max <dedalus123@gmail.com>
2018-08-26 12:09:18 +0200

    designing new game scene

--- src/models/player.ts
+++ src/models/player.ts
@@ -2,12 +2,15 @@ import { Combatant, CombatantSide } from "./combatant";
 import { Armor } from "./armor";
 import { ArmorService } from "../services/armor.service";
 import { Special } from "./special";
+import { Weapon } from "./weapon";
+import { WeaponService } from "../services/weapon.service";
 
 export class Player extends Combatant {
     
     story: string;
     armor: Armor;
     
+    definedWeapons: Array<Weapon>;
     definedSpecials: Array<Special>;
     specialsUsed: number;
     
@@ -24,6 +27,14 @@ export class Player extends Combatant {
         this.story = data.story;
         this.armor = ArmorService.get(data.armor);
         
+        // add all defined weapons for the character
+        this.definedWeapons = [];
+        if (data.weapons && data.weapons.length > 0) {
+            data.weapons.forEach(w => {
+                this.definedWeapons.push(WeaponService.get(w));
+            });
+        }
+
         // remove specials if they are not purchased
         this.specialsUsed = data.specialsUsed;
         this.definedSpecials = this.specials.slice();
--- src/scenes/new-game.scene.ts
+++ src/scenes/new-game.scene.ts
@@ -1,9 +1,25 @@
 import { Assets } from "../models/assets";
 import { Player } from "../models/player";
-import { FONT_FAMILY, Styles, FONT_FAMILY_BLOCK } from "../models/styles";
+import { FONT_FAMILY, FONT_FAMILY_BLOCK } from "../models/styles";
 import { ArrowsService, ArrowOrientation } from "../services/arrows.service";
 import { TextualService } from "../services/textual.service";
 import { Settings } from "../models/settings";
+import { CombatantType } from "../models/combatant";
+
+const TITLE_STYLE = { font: '32px ' + FONT_FAMILY, fill: '#FFEEBC'},
+    BACK_STYLE = { font: '32px ' + FONT_FAMILY, fill: '#581B06', align: 'center', stroke: '#000000', strokeThickness: 2 },
+    START_GAME_STYLE = { font: '32px ' + FONT_FAMILY, fill: '#FF6A00', align: 'center', stroke: '#000000', strokeThickness: 2 },
+    STORY_STYLE = { font: '16px ' + FONT_FAMILY, fill: '#FFFFFF', wordWrap: { width: 640 }},
+    DESCRIPTION_STYLE = { font: '16px ' + FONT_FAMILY, fill: '#BBBBBB', wordWrap: { width: 640 }},
+    TYPE_STYLE = { font: '16px ' + FONT_FAMILY, fill: '#BBBBBB', wordWrap: { width: 640 }},
+    STATS_TITLE_STYLE = { font: '18px ' + FONT_FAMILY_BLOCK, fill: '#FF0000'}, // red technical
+    STATS_VALUE_STYLE = { font: '18px ' + FONT_FAMILY_BLOCK, fill: '#CCA600' }, // gold technical
+    MOVE_STYLE = { font: '18px ' + FONT_FAMILY, fill: '#FF6A00' }, // orange
+    WEAPON_NAME_STYLE = { font: '20px ' + FONT_FAMILY, fill: '#563C24', align: 'center'},
+    WEAPON_DESCRIPTION_STYLE = { font: '16px ' + FONT_FAMILY, fill: '#000', wordWrap: { width: 220 }},
+    SPECIAL_NAME_STYLE = { font: '20px ' + FONT_FAMILY, fill: '#FFEEBC', align: 'center'},
+    SPECIAL_DESCRIPTION_STYLE = { font: '14px ' + FONT_FAMILY_BLOCK, fill: '#BBBBBB', wordWrap: { width: 300 }},
+    MANA_COST_STYLE = { font: '16px ' + FONT_FAMILY_BLOCK, fill: '#B770FF', align: 'center'};
 
 export class NewGameScene extends Phaser.Scene {
 
@@ -16,11 +32,32 @@ export class NewGameScene extends Phaser.Scene {
     private _title: Phaser.GameObjects.Text;
     private _story: Phaser.GameObjects.Text;
     private _description: Phaser.GameObjects.Text;
+    private _type: Phaser.GameObjects.Text;
+    private _health: Phaser.GameObjects.Text;
+    private _attack: Phaser.GameObjects.Text;
+    private _defense: Phaser.GameObjects.Text;
+    private _armor: Phaser.GameObjects.Text;
+    private _weapons: Phaser.GameObjects.Group;
+    private _weaponsTitles: Phaser.GameObjects.Group;
+    private _weaponsDescriptions: Phaser.GameObjects.Group;
+    private _specials: Phaser.GameObjects.Group;
+    private _specialsTitles: Phaser.GameObjects.Group;
+    private _specialsDescriptions: Phaser.GameObjects.Group;
+    private _manaCosts: Phaser.GameObjects.Group;
 
     get getObjects(): Array<Phaser.GameObjects.GameObject> {
-        return [
-            this._image, this._title, this._story, this._description
+        var objects: Array<Phaser.GameObjects.GameObject> = [
+            this._image, this._title, this._story, this._description,
+            this._health, this._attack, this._defense, this._armor
         ];
+        objects = objects.concat(this._weapons.getChildren());
+        objects = objects.concat(this._weaponsTitles.getChildren());
+        objects = objects.concat(this._weaponsDescriptions.getChildren());
+        objects = objects.concat(this._specials.getChildren());
+        objects = objects.concat(this._specialsTitles.getChildren());
+        objects = objects.concat(this._specialsDescriptions.getChildren());
+        objects = objects.concat(this._manaCosts.getChildren());
+        return objects;
     }
 
     constructor() {
@@ -41,11 +78,9 @@ export class NewGameScene extends Phaser.Scene {
 
     create(): void {
         this.cameras.main.fadeIn(1000);
-        this.cameras.main.setBackgroundColor(0x111111);
+        this.cameras.main.setBackgroundColor(0x141414);
 
-        var title = this.add.text(this.cameras.main.width / 2, 20, 'Choose your heroes', {
-            font: '32px ' + FONT_FAMILY, fill: '#FFFFFF'
-        });
+        var title = this.add.text(this.cameras.main.width / 2, 20, 'Choose your heroes', TITLE_STYLE);
         title.setOrigin(0.5, 0.5);
         this.add.tween({
             targets: [ title ],
@@ -58,14 +93,12 @@ export class NewGameScene extends Phaser.Scene {
             repeat: Infinity
         });
 
-        var exit = TextualService.createTextButton(this, 'Back', 50, 30,
-            { font: '32px ' + FONT_FAMILY, fill: '#581B06', align: 'center', stroke: '#000000', strokeThickness: 2 }, a => {
+        var exit = TextualService.createTextButton(this, 'Back', 50, 30, BACK_STYLE, a => {
                 this.sound.add('click', { volume: Settings.sound.sfxVolume }).play();
                 this.scene.start('LoadingScene', { loadScene: 'MainMenuScene' });
             });
 
-        var startGame = TextualService.createTextButton(this, 'Start game', this.cameras.main.width - 100, 30, 
-            { font: '32px ' + FONT_FAMILY, fill: '#FF6A00', align: 'center', stroke: '#000000', strokeThickness: 2 }, a => {
+        var startGame = TextualService.createTextButton(this, 'Start game', this.cameras.main.width - 100, 30, START_GAME_STYLE, a => {
                 if (this._selectedCharacters.length === 3) {
                     this.scene.start('LoadingScene', { loadScene: 'MainMenuScene' });
                 } else {
@@ -106,41 +139,90 @@ export class NewGameScene extends Phaser.Scene {
 
         var startX = this.cameras.main.width / 2 - 120;
         
-        this._title = this.add.text(startX, 60, '', {
-            font: '36px ' + FONT_FAMILY, fill: '#FFEEBC'
-        });
+        // texts
 
-        this._story = this.add.text(startX, 120,'', {
-            font: '18px ' + FONT_FAMILY, fill: '#FFFFFF', wordWrap: { width: 640 }
-        });
+        this._title = this.add.text(startX, 60, '', TITLE_STYLE);
+        this._story = this.add.text(startX, 110,'', STORY_STYLE);
+        this._description = this.add.text(startX, 170,'', DESCRIPTION_STYLE);
+        this._type = this.add.text(this.cameras.main.width - 240, 80,'', TYPE_STYLE);
 
-        this._description = this.add.text(startX, 200,'', {
-            font: '18px ' + FONT_FAMILY, fill: '#DDDDDD', wordWrap: { width: 640 }
-        });
+        // stats
 
-        // health
-        // defense
-        // attack
-        // type
-        // armors
-        // weapons
-        // specials
-
-        var hpTitle = this.add.text(startX, 250, 'HP', {
-            font: '18px ' + FONT_FAMILY_BLOCK, fill: '#FF0000'
-        });
+        var hpTitle = this.add.text(startX, 200, 'HP', STATS_TITLE_STYLE);
         hpTitle.setShadow(1, 1, '#000', 2, false, true);
 
-        var defenseTitle = this.add.text(startX + 80, 250, 'Defense', {
-            font: '18px ' + FONT_FAMILY_BLOCK, fill: '#FF0000'
-        });
-        defenseTitle.setShadow(1, 1, '#000', 2, false, true);
+        this._health = this.add.text(hpTitle.x + hpTitle.width + 5, hpTitle.y, '', STATS_VALUE_STYLE);
 
-        var attackTitle = this.add.text(startX + 160, 250, 'Attack', {
-            font: '18px ' + FONT_FAMILY_BLOCK, fill: '#FF0000'
-        });
+        var attackTitle = this.add.text(startX + 90, 200, 'Attack', STATS_TITLE_STYLE);
         attackTitle.setShadow(1, 1, '#000', 2, false, true);
 
+        this._attack = this.add.text(attackTitle.x + attackTitle.width + 5, attackTitle.y, '', STATS_VALUE_STYLE);
+
+        var defenseTitle = this.add.text(startX + 210, 200, 'Defense', STATS_TITLE_STYLE);
+        defenseTitle.setShadow(1, 1, '#000', 2, false, true);
+
+        this._defense = this.add.text(defenseTitle.x + defenseTitle.width + 5, defenseTitle.y, '', STATS_VALUE_STYLE);
+
+        var armorTitle = this.add.text(startX + 320, 200, 'Armor', STATS_TITLE_STYLE);
+        armorTitle.setShadow(1, 1, '#000', 2, false, true);
+
+        this._armor = this.add.text(armorTitle.x + armorTitle.width + 5, armorTitle.y, '', STATS_VALUE_STYLE);
+
+        // weapons
+
+        var backgroundPaper = this.add.sprite(startX - 40, 220, 'paper');
+        backgroundPaper.setOrigin(0, 0);
+        backgroundPaper.setScale(0.42, 0.45);
+
+        var weapons = this.add.text(startX, 240, 'Weapons', MOVE_STYLE);
+        weapons.setShadow(1, 1, '#000', 2, false, true);
+
+        this._weapons = this.add.group();
+        for (var i = 0; i < 4; i++) { // TODO: this is now fixed to 4 weapons per character!
+            var weapon = this.add.sprite(weapons.x + 40, weapons.y + 80 + 80 * i, null);
+            weapon.setScale(0.7);
+            weapon.setOrigin(0.5, 0.5);
+            this._weapons.add(weapon, true);
+        }
+        this._weaponsTitles = this.add.group();
+        for (var i = 0; i < 4; i++) { // TODO: this is now fixed to 4 weapons per character!
+            var weaponText = this.add.text(weapons.x + 90, weapons.y + 40 + 80 * i, '', WEAPON_NAME_STYLE);
+            this._weaponsTitles.add(weaponText, true);
+        }
+        this._weaponsDescriptions = this.add.group();
+        for (var i = 0; i < 4; i++) { // TODO: this is now fixed to 4 weapons per character!
+            var weaponDesc = this.add.text(weapons.x + 90, weapons.y + 60 + 80 * i, '', WEAPON_DESCRIPTION_STYLE);
+            this._weaponsDescriptions.add(weaponDesc, true);
+        }
+
+        // specials
+
+        var specials = this.add.text(startX + 360, 230, 'Specials', MOVE_STYLE);
+        specials.setShadow(1, 1, '#000', 2, false, true);
+
+        this._specials = this.add.group();
+        for (var i = 0; i < 4; i++) { // TODO: this is now fixed to 4 specials per character!
+            var card = this.add.sprite(specials.x + 30, specials.y + 60 + 90 * i, 'special-card');
+            card.setScale(0.8);
+            var special = this.add.sprite(specials.x + 30, specials.y + 60 + 90 * i, null);
+            special.setScale(0.8);
+            this._specials.add(special, true);
+        }
+        this._specialsTitles = this.add.group();
+        for (var i = 0; i < 4; i++) { // TODO: this is now fixed to 4 specials per character!
+            var spText = this.add.text(specials.x + 80, specials.y + 25 + 90 * i, '', SPECIAL_NAME_STYLE);
+            this._specialsTitles.add(spText, true);
+        }
+        this._specialsDescriptions = this.add.group();
+        for (var i = 0; i < 4; i++) { // TODO: this is now fixed to 4 specials per character!
+            var spDesc = this.add.text(specials.x + 80, specials.y + 45 + 90 * i, '', SPECIAL_DESCRIPTION_STYLE);
+            this._specialsDescriptions.add(spDesc, true);
+        }
+        this._manaCosts = this.add.group();
+        for (var i = 0; i < 4; i++) { // TODO: this is now fixed to 4 specials per character!
+            var manaCost = this.add.text(specials.x + 80, specials.y + 75 + 90 * i, '', MANA_COST_STYLE);
+            this._manaCosts.add(manaCost, true);
+        }
     }
 
     private displayCharacter() {
@@ -153,6 +235,36 @@ export class NewGameScene extends Phaser.Scene {
         this._title.setText(player.title);        
         this._story.setText(player.story);
         this._description.setText(player.description);
+        this._type.setText(CombatantType[player.type]);
+        this._health.setText(player.health.toString());
+        this._attack.setText(player.baseAttack.toString());
+        this._defense.setText(player.baseDefense.toString());
+        this._armor.setText(player.armor.title + ' (+' + player.armor.defense + ')');
+
+        for (var i = 0; i < 4; i++) { // TODO: this is now fixed to 4 weapons per character!
+            var weapon = this._weapons.getChildren()[i] as Phaser.GameObjects.Sprite;
+            weapon.setTexture('weapons/' + player.definedWeapons[i].name);
+
+            var weaponTitle = this._weaponsTitles.getChildren()[i] as Phaser.GameObjects.Text;
+            weaponTitle.setText(player.definedWeapons[i].title);
+
+            var weaponDesc = this._weaponsDescriptions.getChildren()[i] as Phaser.GameObjects.Text;
+            weaponDesc.setText(player.definedWeapons[i].description + ' (' + player.definedWeapons[i].modifier + ' Att)');
+        }
+
+        for (var i = 0; i < 4; i++) { // TODO: this is now fixed to 4 specials per character!
+            var special = this._specials.getChildren()[i] as Phaser.GameObjects.Sprite;
+            special.setTexture('specials/' + player.definedSpecials[i].name);
+
+            var spTitle = this._specialsTitles.getChildren()[i] as Phaser.GameObjects.Text;
+            spTitle.setText(player.definedSpecials[i].title);
+
+            var spDesc = this._specialsDescriptions.getChildren()[i] as Phaser.GameObjects.Text;
+            spDesc.setText(player.definedSpecials[i].description);
+
+            var manaCost = this._manaCosts.getChildren()[i] as Phaser.GameObjects.Text;
+            manaCost.setText('Mana cost: ' + player.definedSpecials[i].manaCost);
+        }
 
         this.add.tween({
             targets: this.getObjects,
