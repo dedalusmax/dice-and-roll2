d45e584f678a0dfe66e4820e795876816fb7e6c8
Dedalus Max <dedalus123@gmail.com>
2018-09-05 19:04:09 +0200

    corpse, fey and new items

--- src/scenes/main-menu.scene.ts
+++ src/scenes/main-menu.scene.ts
@@ -145,7 +145,7 @@ export class MainMenuScene extends Phaser.Scene {
         options.playerParty = party;
         options.terrain = 'beach';
         options.skirmish = true;
-        options.enemyParty = [ Assets.monsters.hoblum, Assets.monsters.serpent ], // [ Assets.monsters.seabound_sailor, Assets.monsters.seabound_captain, Assets.monsters.siren ];
+        options.enemyParty = [ Assets.monsters.fey, Assets.monsters.corpse, Assets.monsters.hoblum ], // [ Assets.monsters.seabound_sailor, Assets.monsters.seabound_captain, Assets.monsters.siren ];
         options.enemyMana = 100;
 
         // TODO: just for testing
