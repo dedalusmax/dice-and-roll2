9f77bb91017a71e3b9b31c84f8ba765e268e4be5
Dedalus Max <dedalus123@gmail.com>
2018-10-04 23:10:55 +0200

    next cutscene designed

--- src/scenes/intro.scene.ts
+++ src/scenes/intro.scene.ts
@@ -7,7 +7,8 @@ import { FONT_FAMILY } from "../models/styles";
 import { ImageService } from "../services/image.service";
 import { IntroSceneOptions } from "./scene-options";
 
-const TITLE_STYLE = { font: '18px ' + FONT_FAMILY, fill: '#FFEEBC'},
+const NARRATION_LIGHT_STYLE = { font: '18px ' + FONT_FAMILY, fill: '#FFEEBC'},
+    NARRATION_DARK_STYLE = { font: '18px ' + FONT_FAMILY, fill: '#581B06'},     
     BACK_STYLE = { font: '32px ' + FONT_FAMILY, fill: '#581B06', align: 'center', stroke: '#000000', strokeThickness: 2 },
     TALK_STYLE = { font: '32px ' + FONT_FAMILY, fill: '#FF6A00', align: 'center', stroke: '#000000', strokeThickness: 2 };
 
@@ -30,6 +31,8 @@ export class IntroScene extends Phaser.Scene {
     }
 
     create(): void {
+        this._timeline = 0;
+
         this.cameras.main.fadeIn(1000);
         this.cameras.main.setBackgroundColor(0x040404);
 
@@ -47,7 +50,7 @@ export class IntroScene extends Phaser.Scene {
 
         switch (this._timeline) {
             case 50:
-                this.displayText('somewhere near the coast of the Fading sea ...');
+                this.displayNarrationTextOnCenter('somewhere near the coast of the Fading sea ...');
                 break;
 
             case 400:
@@ -69,27 +72,27 @@ export class IntroScene extends Phaser.Scene {
                 break;
 
             case 600:
-                this.displayTextOnTopRight('one ship was sailing carrying mysterious passengers ...');
+                this.displayNarrationText('one ship was sailing carrying mysterious passengers ...', NARRATION_LIGHT_STYLE);
                 break;
 
             case 800:
-                this.displayCharacterOnLeft(0, 'Hey! This storm is getting bigger and bigger.\n We might die out here!!');
+                this.displayCharacter(0, true, 'Hey! This storm is getting bigger and bigger.\n We might die out here!!');
                 break;
 
             case 1100:
-                this.removeCharacterOnLeft();
+                this.removeCharacter(true);
                 break;
 
             case 1150:
-                this.displayTextOnTopRight('the sea was treacherous, and the ship swang on the waves helplesly ...');
+                this.displayNarrationText('the sea was treacherous, and the ship swang on the waves helplesly ...', NARRATION_LIGHT_STYLE);
                 break;
 
             case 1300:
-                this.displayCharacterOnRight(1, 'Hold tight! A large wave is coming!\n We\'re gonna drown!');
+                this.displayCharacter(1, false, 'Hold tight! A large wave is coming!\n We\'re gonna drown!');
                 break;
 
             case 1600:
-                this.removeCharacterOnRight();
+                this.removeCharacter(false);
                 break;
 
             case 1800:
@@ -105,6 +108,18 @@ export class IntroScene extends Phaser.Scene {
                 this._ambientMusic = this.sound.add('ambient-beach', { volume: Settings.sound.musicVolume });
                 this._ambientMusic.play('', { loop: true });
                 break;
+
+            case 2200:
+                this.displayNarrationText('the ship crashed on the shores of small peninsula\n so distant from their destination', NARRATION_DARK_STYLE);
+                break;
+
+            case 2400:
+                this.displayCharacter(2, true, 'Well, that was close!\n Next time I\'m bringing swimming gear');
+                break;
+
+            case 2600:
+                this.removeCharacter(true);
+                break;
         }
 
         if (this._timeline >= 1800 && this._timeline < 2000 && this.sound.volume > 0) {
@@ -112,55 +127,62 @@ export class IntroScene extends Phaser.Scene {
         }
     }
 
-    private displayText(text: string) {
-        this._centeredText = this.add.text(this.cameras.main.width / 2, this.cameras.main.height / 2, text, TITLE_STYLE);
+    private displayNarrationTextOnCenter(text: string) {
+        this._centeredText = this.add.text(this.cameras.main.width / 2, this.cameras.main.height / 2, text, NARRATION_LIGHT_STYLE);
         this._centeredText.setOrigin(0.5, 0.5);    
         this._centeredText.alpha = 0;
+        this.applyTweenToText();
+    }
+
+    private displayNarrationText(text: string, style: any) {
+        this._centeredText = this.add.text(this.cameras.main.width * 0.6, 40, text, style);
+        this._centeredText.setOrigin(0.5, 0.5);    
+        this._centeredText.alpha = 0;
+        this.applyTweenToText();
+    }
+
+    private applyTweenToText() {
         this.add.tween({
             targets: [ this._centeredText ],
             ease: 'Linear',
-            duration: 2000,
+            duration: 3000,
             delay: 1000,
             alpha: 1,
             yoyo: true
         });
     }
 
-    private displayTextOnTopRight(text: string) {
-        this._centeredText = this.add.text(this.cameras.main.width * 0.6, 40, text, TITLE_STYLE);
-        this._centeredText.setOrigin(0.5, 0.5);    
-        this._centeredText.alpha = 0;
-        this.add.tween({
-            targets: [ this._centeredText ],
-            ease: 'Linear',
-            duration: 2000,
-            delay: 1000,
-            alpha: 1,
-            yoyo: true
-        });
-    }
-
-    private displayCharacterOnLeft(index: number, text: string) {
-        var position = new Phaser.Geom.Point(-200, 250);
+    private displayCharacter(index: number, left: boolean, text: string) {
+        var leftPosition = new Phaser.Geom.Point(-200, 250);
+        var rightPosition = new Phaser.Geom.Point(2000, 250);
+        let position = left ? leftPosition : rightPosition;
         var name = 'characters/' +  this._options.playerParty.members[index].name;
         this._character = this.add.sprite(position.x, this.cameras.main.height - position.y, name);
         this._character.setOrigin(0.5, 0.5);
         this._character.setAlpha(0);
-        
+        let targetX = left ? 100 : this.cameras.main.width - 200;
         this.add.tween({
             targets: [this._character],
             ease: 'Quad.easeIn',
             duration: 800,
-            x: 100,
+            x: targetX,
             alpha: 1,
             onComplete: () => {
-                this.displayCharacterTextOnLeft(text);
+                if (left) {
+                    this.displayCharacterText(left, text);
+                } else {
+                    this.displayCharacterText(left, text);
+                }
             }
         });
     }
 
-    private displayCharacterTextOnLeft(text: string) {
-        this._centeredText = this.add.text(this.cameras.main.width * 0.4, this.cameras.main.height - 100, text, TALK_STYLE);
+    private displayCharacterText(left: boolean, text: string) {
+        if (left) {
+            this._centeredText = this.add.text(this.cameras.main.width * 0.4, this.cameras.main.height - 100, text, TALK_STYLE);
+        } else {
+            this._centeredText = this.add.text(this.cameras.main.width * 0.6, this.cameras.main.height - 100, text, TALK_STYLE);
+        }
         this._centeredText.setOrigin(0.5, 0.5);    
         this._centeredText.alpha = 0;
         this.add.tween({
@@ -171,57 +193,14 @@ export class IntroScene extends Phaser.Scene {
         });
     }
 
-    private removeCharacterOnLeft() {
+    private removeCharacter(left: boolean) {
         this._centeredText.alpha = 0;
         this.add.tween({
             targets: [this._character],
             ease: 'Quad.easeIn',
             duration: 800,
             delay: 200,
-            x: -200,
-            alpha: 0
-        });
-    }
-
-    private displayCharacterOnRight(index: number, text: string) {
-        var position = new Phaser.Geom.Point(2000, 250);
-        var name = 'characters/' +  this._options.playerParty.members[index].name;
-        this._character = this.add.sprite(position.x, this.cameras.main.height - position.y, name);
-        this._character.setOrigin(0.5, 0.5);
-        this._character.setAlpha(0);
-        
-        this.add.tween({
-            targets: [this._character],
-            ease: 'Quad.easeIn',
-            duration: 800,
-            x: this.cameras.main.width - 200,
-            alpha: 1,
-            onComplete: () => {
-                this.displayCharacterTextOnRight(text);
-            }
-        });
-    }
-
-    private displayCharacterTextOnRight(text: string) {
-        this._centeredText = this.add.text(this.cameras.main.width * 0.6, this.cameras.main.height - 100, text, TALK_STYLE);
-        this._centeredText.setOrigin(0.5, 0.5);    
-        this._centeredText.alpha = 0;
-        this.add.tween({
-            targets: [ this._centeredText ],
-            ease: 'Linear',
-            duration: 400,
-            alpha: 1
-        });
-    }
-
-    private removeCharacterOnRight() {
-        this._centeredText.alpha = 0;
-        this.add.tween({
-            targets: [this._character],
-            ease: 'Quad.easeIn',
-            duration: 800,
-            delay: 200,
-            x: 2000,
+            x: left ? -200 : 2000,
             alpha: 0
         });
     }
