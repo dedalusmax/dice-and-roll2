fd3bf5e1b5513605547429e99b4c9a88f7625349
dedalusmax <dedalus123@gmail.com>
2021-12-04 13:18:22 +0100

    prod resolved

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const webpack = require('webpack');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/game',
     mode: 'production',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'app.bundle.js',
+        filename: 'app/app.bundle.js',
         publicPath: '/dist'
     },
 
@@ -30,6 +31,12 @@ module.exports = {
     },
 
     plugins: [
+        new CopyPlugin([
+            { from: 'styles', to: 'styles' },
+            { from: 'assets', to: 'assets' },
+            { from: 'data', to: 'data' },
+            { from: 'index.html', to: 'index.html', toType: 'file'},
+          ]),
         new webpack.DefinePlugin({
             CANVAS_RENDERER: JSON.stringify(true),
             WEBGL_RENDERER: JSON.stringify(true)
